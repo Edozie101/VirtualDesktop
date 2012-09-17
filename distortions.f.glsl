268ac5be02f747c746e652f8b5ed7b821bad8ac6
hesh <hesham.wahba@gmail.com>
2012-09-17 06:40:42 +0300

    - better barrel distortion, much cleaner results

--- distortions.f.glsl
+++ distortions.f.glsl
@@ -8,6 +8,7 @@ uniform sampler2D texture;
 
 varying vec2 texcoord;
 
+// based off of: http://github.prideout.net/barrel-distortion/
 // Given a vec2 in [-1,+1], generate a texture coord in [0,+1]
 vec2 Distort(vec2 p)
 {
@@ -20,16 +21,23 @@ vec2 Distort(vec2 p)
     return 0.5 * (p + 1.0);
 }
 
+// based off of: http://paulbourke.net/miscellaneous/lenscorrection/
+vec2 Distort2(vec2 p)
+{
+	// these factors range from 0 to 0.1 for a wide-angle lens
+	const float ax = 0.1;
+	const float ay = 0.1;
+	
+	float len = length(p);
+	float len_2 = len*len;
+	
+	float xx = len/(1.0 - ax*len_2);
+	float yy = len/(1.0 - ay*len_2);
+	
+	return (p/vec2(1.0-ax*xx*xx,1.0-ay*yy*yy)+1.0)*0.5;
+}
+
 void main()
 {
-	//const float K1 = 0.5;
-	//const float K2 = 0.5;
-	//float r_2 = (texcoord.s-0.5)*(texcoord.s-0.5)+(texcoord.t-0.5)*(texcoord.t-0.5);
-	 
-	//vec2 texcoord2 = vec2(
-	//(texcoord.s-0.5)*(1.0+(K1+K2*r_2)*r_2),
-	//(texcoord.t-0.5)*(1.0+(K1+K2*r_2)*r_2)
-	//);
-	
-    gl_FragColor = texture2D(texture, clamp(Distort(texcoord), 0.0, 1.0));//texcoord2);
+    gl_FragColor = texture2D(texture, clamp(Distort2(texcoord), 0.0, 1.0));
 }
--- ibex.cpp
+++ ibex.cpp
@@ -652,6 +652,8 @@ void prep_framebuffers()
 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0,
                GL_RGBA, GL_UNSIGNED_BYTE, 0);
   glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
