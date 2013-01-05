567fdc7d3f23a4380cc56d30b53550fe99856032
Hesham Wahba <hesham.wahba@gmail.com>
2013-01-05 15:14:27 -0500

    - remove dead code

--- IbexMac/MyWindow.mm
+++ IbexMac/MyWindow.mm
@@ -7,8 +7,6 @@
 //
 
 #import "MyWindow.h"
-#import "ibex.h"
-#import <Carbon/Carbon.h>
 
 @implementation MyWindow
 
@@ -20,51 +18,6 @@
 }
 - (BOOL)canBecomeMainWindow {
     return YES;
-}
-
-- (void)keyDown:(NSEvent *)theEvent {
-    switch(theEvent.keyCode) {
-        case kVK_ANSI_W:
-            walkForward = 1;
-            break;
-        case kVK_ANSI_S:
-            walkForward = -1;
-            break;
-        case kVK_ANSI_A:
-            strafeRight = 1;
-            break;
-        case kVK_ANSI_D:
-            strafeRight = -1;
-            break;
-        case kVK_Space:
-            break;
-    }
-}
-- (void)keyUp:(NSEvent *)theEvent {
-    switch(theEvent.keyCode) {
-        case kVK_ANSI_W:
-            walkForward = 0;
-            break;
-        case kVK_ANSI_S:
-            walkForward = 0;
-            break;
-        case kVK_ANSI_A:
-            strafeRight = 0;
-            break;
-        case kVK_ANSI_D:
-            strafeRight = 0;
-            break;
-        case kVK_Space:
-            break;
-    }
-}
-
-- (void)mouseMoved:(NSEvent*)theEvent {
-    double x = theEvent.deltaX;
-    double y = theEvent.deltaY;
-    
-    relativeMouseX = x;
-    relativeMouseY = y;
 }
 
 @end
