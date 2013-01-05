4fdc5f7da7e364f71ea511cdb165df91b1bba3b9
Hesham Wahba <hesham.wahba@gmail.com>
2013-01-05 15:21:43 -0500

    - more cleanup

--- IbexMac/AppDelegate.h
+++ IbexMac/AppDelegate.h
@@ -8,13 +8,17 @@
 
 #import <Cocoa/Cocoa.h>
 
+@class MyOpenGLView;
+
 extern "C" {
     typedef int CGSConnectionID;
     CGError CGSSetConnectionProperty(CGSConnectionID cid, CGSConnectionID targetCID, CFStringRef key, CFTypeRef value);
     int _CGSDefaultConnection();
 }
 
-@interface AppDelegate : NSObject <NSApplicationDelegate,NSWindowDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate,NSWindowDelegate> {
+    MyOpenGLView *fullScreenView;
+}
 
 @property (assign) IBOutlet NSWindow *window;
 
--- IbexMac/AppDelegate.mm
+++ IbexMac/AppDelegate.mm
@@ -30,37 +30,15 @@
     NSLog(@"%@", NSStringFromRect(r));
     
     [_window setStyleMask:NSBorderlessWindowMask];
-    [_window setAcceptsMouseMovedEvents:YES];
-    [_window setMovableByWindowBackground:YES];
-//    [_window setLevel:NSNormalWindowLevel];
-    [_window setExcludedFromWindowsMenu:YES];
     [_window setLevel:NSScreenSaverWindowLevel];
-//    [_window setLevel:CGShieldingWindowLevel()+1];
-        [_window setFrame:NSScreen.mainScreen.frame display:YES];
-//    [_window toggleFullScreen:self];
-    [_window makeKeyAndOrderFront:nil];
-    
-//    [_window setContentSize:r.size];
-//    [_window setFrameOrigin:NSPointFromCGPoint(CGPointMake(10,20))];
+    [_window setFrame:NSScreen.mainScreen.frame display:YES];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-//    [NSCursor hide];
-    CGDisplayHideCursor(kCGDirectMainDisplay);
-    
-//    CFStringRef propertyString = CFStringCreateWithCString(NULL, "SetsCursorInBackground", kCFStringEncodingMacRoman);
-//    CGSSetConnectionProperty(_CGSDefaultConnection(), _CGSDefaultConnection(), propertyString, kCFBooleanTrue);
-//    CFRelease(propertyString);
-//    CGDisplayHideCursor(kCGDirectMainDisplay);
-    
-//    [_window setIgnoresMouseEvents:YES];
-//    [_window setAcceptsMouseMovedEvents:YES];
-    [_window setLevel:NSScreenSaverWindowLevel];
-    
     CGRect mainDisplayRect = NSScreen.mainScreen.frame;
     
-    
+    CGDisplayHideCursor(kCGDirectMainDisplay);
     
     NSOpenGLPixelFormatAttribute attrs[] =
     {
@@ -70,79 +48,15 @@
     NSOpenGLPixelFormat* pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
     
     NSRect viewRect = NSMakeRect(0.0, 0.0, mainDisplayRect.size.width, mainDisplayRect.size.height);
-    MyOpenGLView *fullScreenView = [[MyOpenGLView alloc] initWithFrame:viewRect pixelFormat: pixelFormat];
+    fullScreenView = [[MyOpenGLView alloc] initWithFrame:viewRect pixelFormat: pixelFormat];
+    
     [_window setContentView: fullScreenView];
     [_window makeKeyAndOrderFront:self];
     [_window makeFirstResponder:fullScreenView];
     
-    return;
-    // Insert code here to initialize your application
-    char *text = "Hello, World!";
-    CGDirectDisplayID display = kCGDirectMainDisplay; // 1
-    
-    
-    const int MAX_DISPLAYS = 5;
-    CGDirectDisplayID displays[MAX_DISPLAYS];
-    uint32_t numDisplays;
-    
-    CGGetActiveDisplayList(MAX_DISPLAYS, displays, &numDisplays);
-    display = displays[0];
-    display = CGMainDisplayID();
-    
-            CGRect r = NSScreen.mainScreen.frame;
-    
-    
-    CFArrayRef a = CGWindowListCreate(
-                                              //CGWindowListOption option,
-                                              kCGWindowListOptionOnScreenOnly,
-                                              kCGNullWindowID
-                                              //                                                      CGWindowID relativeToWindow
-                                      );
-    sleep(1);
-    
-//    CGError err = CGDisplayCapture (display); // 2
-    CGError err = CGDisplayCaptureWithOptions(display, kCGCaptureNoFill);
-//    CGError err = CGCaptureAllDisplaysWithOptions(kCGCaptureNoFill);
-    
-    if (err == kCGErrorSuccess)
-    {
-//        CGSize s = CGDisplayScreenSize (display);
-
-        CGContextRef ctx = CGDisplayGetDrawingContext (display); // 3
-        if (ctx != NULL)
-        {
-            for(int i2 = 0; i2 < 50; ++ i2) {
-
-                CGImageRef i = CGWindowListCreateImageFromArray(
-                                                                CGRectInfinite,
-                                                                a,
-                                                                kCGWindowImageDefault
-                                                                );
-//                CGImageRef i = CGWindowListCreateImage(CGRectInfinite,
-//                                                       kCGWindowListOptionOnScreenOnly,//kCGWindowListOptionAll,
-//                                                       kCGNullWindowID,kCGWindowImageDefault);
-                
-//            CGImageRef i = CGDisplayCreateImage(display);
-            CGContextDrawImage(ctx, r, i);
-            CGImageRelease(i);
-                
-                struct timespec tim, tim2;
-                tim.tv_sec  = 0;
-                tim.tv_nsec = 50000000L;
-                nanosleep(&tim,&tim2);
-            }
-            
-//            CGContextSelectFont (ctx, "Times-Roman", 48, kCGEncodingMacRoman);
-//            CGContextSetTextDrawingMode (ctx, kCGTextFillStroke);
-//            CGContextSetRGBFillColor (ctx, 1, 1, 1, 0.75);
-//            CGContextSetRGBStrokeColor (ctx, 1, 1, 1, 0.75);
-//            CGContextShowTextAtPoint (ctx, 40, 40, text, strlen(text)); // 4
-//            sleep (4); // 5
-        }
-        CGDisplayRelease (display); // 6
-    }
-    
-//                NSLog(@"%@", r);
+    [_window setAcceptsMouseMovedEvents:YES];
+    [_window setMovableByWindowBackground:YES];
+    [_window setExcludedFromWindowsMenu:YES];
 }
 
 @end
