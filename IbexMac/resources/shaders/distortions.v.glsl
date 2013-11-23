5c74366df487db6f03d926a3b7dd7504f987790e
Hesham Wahba <hesham.wahba@gmail.com>
2013-11-22 19:57:27 -0500

    - delete commented out code in shader

--- IbexMac/resources/shaders/distortions.v.glsl
+++ IbexMac/resources/shaders/distortions.v.glsl
@@ -7,6 +7,6 @@ varying vec2 oTexCoord;
 
 void main()
 {
-    oTexCoord = a_texCoord.xy; //gl_TexCoord = a_texCoord;//gl_MultiTexCoord0;
-	gl_Position = a_position;//gl_Vertex;
+    oTexCoord = a_texCoord.xy;
+	gl_Position = a_position;
 }
