8e3ed151e0420f4f6530f1fd6d6aa36f80982410
Hesham Wahba <hesham.wahba@gmail.com>
2013-11-24 15:59:17 -0500

    - header and include cleanups

--- IbexMac/RendererPlugin.cpp
+++ IbexMac/RendererPlugin.cpp
@@ -1,45 +1,5 @@
 #include "RendererPlugin.h"
 
-// --- OpenGL ----------------------------------------------------------------
-#define GLX_GLXEXT_PROTOTYPES
-
-#ifdef __APPLE__
-
-#ifndef MY_OPENGL_DEFS
-#define MY_OPENGL_DEFS
-#include "GL/glew.h"
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#include <OpenGL/glext.h>
-#endif
-
-typedef unsigned long Window;
-typedef unsigned long GLXContext;
-
-#else
-#ifdef _WIN32
-
-#include "GL/glew.h"
-#include <GL/gl.h>
-#include <GL/glu.h>
-//#include <GL/glext.h>
-
-typedef unsigned long Window;
-typedef unsigned long GLXContext;
-
-#else
-
-#include <GL/glew.h>
-#include <GL/glxew.h>
-#include <GL/gl.h>
-#include <GL/glx.h>
-#include <GL/glext.h>
-#include <GL/glxext.h>
-#include <GL/glu.h>
-
-#endif
-#endif
-
 RendererPlugin::~RendererPlugin() {
 
 }
