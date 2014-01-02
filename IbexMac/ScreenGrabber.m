8f24b2668ef8b1604d36e117a644f43a2e3e8ef9
Hesham Wahba <hesham.wahba@gmail.com>
2014-01-01 22:45:27 -0500

    - delete commented out code

--- IbexMac/ScreenGrabber.m
+++ IbexMac/ScreenGrabber.m
@@ -16,11 +16,9 @@ static NSTimer *t;
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code here.
         t = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(refresh) userInfo:nil repeats:YES];
         
-        display = kCGDirectMainDisplay; // 1
-//        CGError err = CGDisplayCaptureWithOptions(display, kCGCaptureNoFill);
+        display = kCGDirectMainDisplay;
     }
     
     return self;
@@ -31,23 +29,21 @@ static NSTimer *t;
     if (self) {
         t = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(refresh) userInfo:nil repeats:YES];
         
-        display = kCGDirectMainDisplay; // 1
-//        CGError err = CGDisplayCaptureWithOptions(display, kCGCaptureNoFill);
+        display = kCGDirectMainDisplay;
     }
     return self;
 }
-
 - (id)initWithCoder:(NSCoder *)coder
 {
     self = [super initWithCoder:coder];
     if (self) {
         t = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(refresh) userInfo:nil repeats:YES];
         
-        display = kCGDirectMainDisplay; // 1
-//        CGError err = CGDisplayCaptureWithOptions(display, kCGCaptureNoFill);
+        display = kCGDirectMainDisplay;
     }
     return self;
 }
+
 - (void) refresh {
     [self setNeedsDisplay:YES];
 }
@@ -55,75 +51,20 @@ static NSTimer *t;
 CGContextRef ctx = NULL;
 - (void)drawRect:(NSRect)dirtyRect
 {
-//    //    CGError err = CGCaptureAllDisplaysWithOptions(kCGCaptureNoFill);
-//    
-////    if (err == kCGErrorSuccess)
-//    {
-//        //        CGSize s = CGDisplayScreenSize (display);
-//        
-//        CGContextRef ctx = CGDisplayGetDrawingContext (display); // 3
-//        if (ctx != NULL)
-//        {
-//            CFArrayRef a = CGWindowListCreate(
-//                                              //CGWindowListOption option,
-//                                              kCGWindowListOptionOnScreenBelowWindow,
-////                                              kCGWindowListOptionAll,
-//                                              (CGWindowID)_window.windowNumber
-//                                              //                                                      CGWindowID relativeToWindow
-//                                              );
-//            
-//            CGImageRef i = CGWindowListCreateImageFromArray(
-//                                                            CGRectInfinite,
-//                                                            a,
-//                                                            kCGWindowImageDefault
-//                                                            );
-//            //                CGImageRef i = CGWindowListCreateImage(CGRectInfinite,
-//            //                                                       kCGWindowListOptionOnScreenOnly,//kCGWindowListOptionAll,
-//            //                                                       kCGNullWindowID,kCGWindowImageDefault);
-//            
-//            //            CGImageRef i = CGDisplayCreateImage(display);
-//            CGRect r = NSScreen.mainScreen.frame;//self.bounds;
-//            CGContextDrawImage(ctx, r, i);
-//            CGImageRelease(i);
-//        }
-//    }
-////    CGDisplayRelease(display);
-//    return;
-    //////
     if (!ctx)
         ctx = [[NSGraphicsContext currentContext] graphicsPort];
-    
-//    CFArrayRef a = CGWindowListCreate(
-//                                      //CGWindowListOption option,
-//                                      kCGWindowListOptionOnScreenOnly,
-//                                      kCGNullWindowID
-//                                      //                                                      CGWindowID relativeToWindow
-//                                      );
-    CFArrayRef a = CGWindowListCreate(
-                                      //CGWindowListOption option,
-                                      kCGWindowListOptionOnScreenBelowWindow,
+
+    CFArrayRef a = CGWindowListCreate(kCGWindowListOptionOnScreenBelowWindow,
                                       (CGWindowID)_window.windowNumber
-                                      //                                                      CGWindowID relativeToWindow
                                       );
-    
-    CGImageRef i = CGWindowListCreateImageFromArray(
-                                                    CGRectInfinite,
+    CGImageRef i = CGWindowListCreateImageFromArray(CGRectInfinite,
                                                     a,
                                                     kCGWindowImageDefault
                                                     );
-    
     CFRelease(a);
-    //                CGImageRef i = CGWindowListCreateImage(CGRectInfinite,
-    //                                                       kCGWindowListOptionOnScreenOnly,//kCGWindowListOptionAll,
-    //                                                       kCGNullWindowID,kCGWindowImageDefault);
-    
-    //            CGImageRef i = CGDisplayCreateImage(display);
-    CGRect r = NSScreen.mainScreen.frame;//self.bounds;
-    CGContextDrawImage(ctx, r, i);
-//    CGContextFillRect(ctx, r);
+
+    CGContextDrawImage(ctx, NSScreen.mainScreen.frame, i);
     CGImageRelease(i);
-    
-//    [self setNeedsDisplay:YES];
 }
 
 @end
