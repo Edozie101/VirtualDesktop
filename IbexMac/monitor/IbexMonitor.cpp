541fb93870930322fc161eebb5345abaefc34274
Hesham Wahba <hesham.wahba@gmail.com>
2014-01-23 07:29:29 -0500

    - use Rectangle class for rendering monitor to reduce code

--- IbexMac/monitor/IbexMonitor.cpp
+++ IbexMac/monitor/IbexMonitor.cpp
@@ -16,6 +16,8 @@
 #include "../ibex.h"
 #include "../simpleworld_plugin/SimpleWorldRendererPlugin.h"
 
+#include "../simpleworld_plugin/Rectangle.h"
+
 std::condition_variable screenshotCondition;
 
 #ifdef __APPLE__
@@ -127,117 +129,20 @@ void Ibex::IbexMonitor::initializeTextures() {
 
 void Ibex::IbexMonitor::renderIbexDisplayFlat(const glm::mat4 &MVP, const glm::mat4 &V, const glm::mat4 &M, bool shadowPass, const glm::mat4 &depthMVP)
 {
-	static GLuint vaoIbexDisplayFlat = 0;
-	//static const GLfloat IbexDisplayFlatScale = 1.0f;//10;
-
-	static GLint IbexDisplayFlatUniformLocations[7] = { 0, 0, 0, 0, 0, 0, 0};
-	static GLint IbexDisplayFlatAttribLocations[3] = { 0, 0, 0 };
-
-	static GLfloat IbexDisplayFlatVertices[] = {
-		0.0, 0, 0.0, 0, 0, -1, 0, 1,
-		1.0, 0.0, 0.0, 0, 0, -1, 1, 1,
-		1.0, -1.0, 0.0, 0, 0, -1, 1, 0,
-		0.0, -1.0, 0.0, 0, 0, -1, 0, 0,
-	};
-	static GLuint vboIbexDisplayFlatVertices = 0;
-
-	static GLushort IbexDisplayFlatIndices[] = {
-		0, 1, 2,
-		0, 2, 3
-	};
-	static GLuint vboIbexDisplayFlatIndices = 0;
-
-	static bool first = true;
-	if(first) {
-		first = false;
-
-		//for(int i = 0; i < sizeof(IbexDisplayFlatVertices)/sizeof(GLfloat); ++i) {
-		//	if(i%8 < 3)
-		//		IbexDisplayFlatVertices[i] *= IbexDisplayFlatScale;
-		//	//if(i%8 == 1)
-		//	//	IbexDisplayFlatVertices[i] *= height/width;
-		//}
-
-		if(standardShaderProgram.shader.program == 0) standardShaderProgram.loadShaderProgram(mResourcePath, "/resources/shaders/emissive.v.glsl", "/resources/shaders/emissive.f.glsl");
-		glUseProgram(standardShaderProgram.shader.program);
-
-
-		IbexDisplayFlatUniformLocations[0] = glGetUniformLocation(standardShaderProgram.shader.program, "MVP");
-		IbexDisplayFlatUniformLocations[1] = glGetUniformLocation(standardShaderProgram.shader.program, "V");
-		IbexDisplayFlatUniformLocations[2] = glGetUniformLocation(standardShaderProgram.shader.program, "M");
-		IbexDisplayFlatUniformLocations[3] = glGetUniformLocation(standardShaderProgram.shader.program, "textureIn");
-		IbexDisplayFlatUniformLocations[4] = glGetUniformLocation(standardShaderProgram.shader.program, "MV");
-		IbexDisplayFlatUniformLocations[5] = glGetUniformLocation(standardShaderProgram.shader.program, "inFade");
-		IbexDisplayFlatUniformLocations[6] = glGetUniformLocation(standardShaderProgram.shader.program, "offset");
-
-		IbexDisplayFlatAttribLocations[0] = glGetAttribLocation(standardShaderProgram.shader.program, "vertexPosition_modelspace");
-		IbexDisplayFlatAttribLocations[1] = glGetAttribLocation(standardShaderProgram.shader.program, "vertexNormal_modelspace");
-		IbexDisplayFlatAttribLocations[2] = glGetAttribLocation(standardShaderProgram.shader.program, "vertexUV");
-
-		glUseProgram(0);
-
-		std::cerr << "setup_buffers" << std::endl;
-		checkForErrors();
-		glGenVertexArrays(1,&vaoIbexDisplayFlat);
-
-		checkForErrors();
-		std::cerr << "gen vaoIbexDisplayFlat done" << std::endl;
-
-		glBindVertexArray(vaoIbexDisplayFlat);
-		glGenBuffers(1, &vboIbexDisplayFlatVertices);
-		glBindBuffer(GL_ARRAY_BUFFER, vboIbexDisplayFlatVertices);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(IbexDisplayFlatVertices), IbexDisplayFlatVertices, GL_STATIC_DRAW);
-
-		glEnableVertexAttribArray(IbexDisplayFlatAttribLocations[0]);
-		glVertexAttribPointer(IbexDisplayFlatAttribLocations[0], 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat)*8, 0);
-		glEnableVertexAttribArray(IbexDisplayFlatAttribLocations[2]);
-		glVertexAttribPointer(IbexDisplayFlatAttribLocations[2], 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat)*8, (GLvoid*) (sizeof(GLfloat) * 6));
-
-
-		glGenBuffers(1, &vboIbexDisplayFlatIndices);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIbexDisplayFlatIndices);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(IbexDisplayFlatIndices), IbexDisplayFlatIndices, GL_STATIC_DRAW);
-	}
-
+    static ::Ibex::Rectangle rectangle(0.5f);
 	for(int i = 0; i < desktopTextures.size(); ++i) {
-		float x = 0;
-		float y = 0;
-		float w = 1;
-		float h = 1;
-
-		const RECT &r = desktopRects[i];
-		x = r.left;
-		y = -r.top;
-		w = r.right-r.left;
-		h = r.bottom-r.top;
+        const RECT &r = desktopRects[i];
+        const float w = r.right-r.left;
+        const float h = r.bottom-r.top;
+        const float x = r.left+w/2.0f;
+        const float y = -r.top-h/2.0f;
         
-        glm::mat4 translate = glm::translate((x-initialOffsetX)/100.0f, (y+initialOffsetY)/100.0f, 0.0f);
-		glm::mat4 scale = glm::scale(w/100.0f, h/100.0f, 1.0f);
-		glm::mat4 MVP2(MVP*translate*scale);
-		glm::mat4 M2(M*translate*scale);
-
-		if(shadowPass) {
-			glUseProgram(shadowProgram.shader.program);
-			glUniformMatrix4fv(ShadowUniformLocations[0], 1, GL_FALSE, &MVP2[0][0]);
-		} else {
-			glUseProgram(standardShaderProgram.shader.program);
-			glUniformMatrix4fv(IbexDisplayFlatUniformLocations[0], 1, GL_FALSE, &MVP2[0][0]);
-			glUniformMatrix4fv(IbexDisplayFlatUniformLocations[1], 1, GL_FALSE, &V[0][0]);
-			glUniformMatrix4fv(IbexDisplayFlatUniformLocations[2], 1, GL_FALSE, &M2[0][0]);
-			glUniformMatrix4fv(IbexDisplayFlatUniformLocations[4], 1, GL_FALSE, &(V*M2)[0][0]);
-
-			if(IbexDisplayFlatUniformLocations[5] >= 0) glUniform1f(IbexDisplayFlatUniformLocations[5], 1.0);
-			if(IbexDisplayFlatUniformLocations[6] >= 0) {
-				glUniform2f(IbexDisplayFlatUniformLocations[6], 0,0);
-			}
-
-			glActiveTexture(GL_TEXTURE0);
-			glBindTexture(GL_TEXTURE_2D, desktopTextures[i]);
-			glUniform1i(IbexDisplayFlatUniformLocations[3], 0);
-		}
-
-		glBindVertexArray(vaoIbexDisplayFlat);
-		glDrawElements(GL_TRIANGLES, sizeof(IbexDisplayFlatIndices)/sizeof(GLushort), GL_UNSIGNED_SHORT, 0);
+        const glm::mat4 translate = glm::translate((x-initialOffsetX)/100.0f, (y+initialOffsetY)/100.0f, 0.0f);
+        const glm::mat4 scale = glm::scale(w/100.0f, h/100.0f*w/h, 1.0f);
+        const glm::mat4 MVP2(MVP*translate*scale);
+        const glm::mat4 M2(M*translate*scale);
+        
+        rectangle.render(MVP2, V, M2, shadowPass, depthMVP, desktopTextures[i], false, 0);
 	}
 }
 
